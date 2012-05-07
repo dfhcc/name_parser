a1730d47dc86c7cd6a387cdb61977e999961a32e
Tom Leonard <thomas_leonard@dfci.harvard.edu>
2012-05-07 11:04:00 -0400

    Add base attributes to PersonName, initializer and some name cleanup methods.

--- lib/parsely/person_name.rb
+++ lib/parsely/person_name.rb
@@ -2,6 +2,35 @@ module Parsely
 
   class PersonName
 
+    ILLEGAL_CHARACTERS = /[^A-Za-z0-9\-\'\.&\/ \,]/
+    REPEATING_SPACES = /\s+/
+
+    attr_reader :name
+    attr_reader :original
+    
+    def initialize(name)
+      @name = name.dup
+      @original = name
+    end
+
+    def clean
+      remove_illegal_characters
+      remove_repeating_spaces
+      remove_leading_and_trailing_spaces
+    end
+
+    def remove_illegal_characters
+      name.gsub!(ILLEGAL_CHARACTERS, '')
+    end
+
+    def remove_repeating_spaces
+      name.gsub!(REPEATING_SPACES, ' ')
+    end
+
+    def remove_leading_and_trailing_spaces
+      name.strip!
+    end
+
   end
 
 end
--- spec/parsely/person_name_spec.rb
+++ spec/parsely/person_name_spec.rb
@@ -1,9 +1,77 @@
 require 'parsely'
 
 describe Parsely::PersonName do
+  let(:name) { 'Horatio Xavier Hornblower' }
+  let(:ppn) { Parsely::PersonName.new(name) }
 
-  it 'should flunk because there are no tests' do
-    raise 'death from above'
+  describe 'name attribute' do 
+    it 'is read only' do
+      ppn.methods.should_not include(:name=) 
+    end
+
+    it 'is set on initialize' do
+      ppn.name.should == name 
+    end
+
+    it 'is shallow copy of original attr' do
+      ppn.name.should == ppn.original
+      ppn.instance_variable_set(:@name, 'changed')
+      ppn.name.should_not == ppn.original
+    end
+
   end
-  
+
+  describe 'original attribute' do
+    it 'is read only' do
+      ppn.methods.should_not include(:original=)
+    end
+
+    it 'is set on initialize' do
+      ppn.original.should == name
+    end
+  end
+
+  describe '#clean' do
+    after do
+      ppn.clean
+    end
+
+    it 'calls #remove_illegal_characters' do
+      ppn.should_receive(:remove_illegal_characters)
+    end
+
+    it 'calls #remove_repeating_spaces' do
+      ppn.should_receive(:remove_repeating_spaces)
+    end
+    
+    it 'calls #remove_leading_and_trailing_spaces' do
+      ppn.should_receive(:remove_leading_and_trailing_spaces)
+    end
+  end
+
+  describe '#remove_illegal_characters' do
+    it 'only allows alpha-numerics, dashes, backslashes, apostrophes and ampersands' do
+      ppn.instance_variable_set(:@name, "aZ1/&'`!@$#%^*()_+=[]{}|\:;""")
+      ppn.remove_illegal_characters
+      ppn.name.should == "aZ1/&'"
+    end
+  end
+
+  describe '#remove_repeating_spaces' do
+    it 'replaces all repeating spaces, tabs and line breaks with a single space' do
+      ppn.instance_variable_set(:@name, "a  b   c\td\n")
+      ppn.remove_repeating_spaces.should == 'a b c d '
+    end
+  end
+
+  describe '#remove_leading_and_trailing_spaces' do
+
+    it 'removes leading and trailing spaces' do
+      ppn.instance_variable_set(:@name, ' a ')
+      ppn.remove_leading_and_trailing_spaces
+      ppn.name.should == 'a'
+    end
+
+  end
+
 end
