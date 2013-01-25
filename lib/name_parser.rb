b9b5bb621dfe484748cffeb750842e52384e80b0
Scott Pullen <s.pullen05@gmail.com>
2013-01-25 10:08:16 -0500

    adding a mixin method for parsing names

--- lib/name_parser.rb
+++ lib/name_parser.rb
@@ -2,4 +2,10 @@ module NameParser
   autoload :Version, 'name_parser/version'
   autoload :Patterns,'name_parser/patterns'
   autoload :Parser,  'name_parser/parser'
+
+  def parse_name(name)
+    parser = Parser.new(name)
+    parser.run
+    parser
+  end
 end
--- spec/name_parser_spec.rb
+++ spec/name_parser_spec.rb
@@ -1,4 +1,23 @@
-require 'name_parser'
+require 'spec_helper'
+
+class TestClass
+  include NameParser
+end
 
 describe NameParser do
+  let!(:name) { "Adams, John Quincy" }
+  let!(:test_class) { TestClass.new }
+
+  describe '#parse_name' do
+    it 'returns a new NameParser::Parser object' do
+      test_class.parse_name(name).class.should == NameParser::Parser
+    end
+
+    it 'should run the parser' do
+      parser = test_class.parse_name(name)
+      parser.first.should == 'John'
+      parser.middle.should == 'Quincy'
+      parser.last.should == 'Adams'
+    end
+  end
 end
