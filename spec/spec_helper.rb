08f0c1aa42ea7763a025f6c2ff75df3ddfbdab00
Scott Pullen <s.pullen05@gmail.com>
2013-02-07 09:59:41 -0500

    putting a condition on when debugger should be required

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'rspec'
-require 'debugger'
+require 'debugger' if RUBY_VERSION < "2.0" && RUBY_VERSION >= "1.9"
 
 $:.push File.expand_path("../lib", __FILE__)
 require 'name_parser'
